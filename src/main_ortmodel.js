87ad4b479cf95cf716dedd9d86f613092a8e5403
Xu Xing <xing.xu@intel.com>
2024-07-15 15:12:52 +0800

    Add open_page_ortmodel.js

---
+++ src/main_ortmodel.js
@@ -0,0 +1,72 @@
+'use strict';
+global.results = {};
+global.results['pass'] = {};
+global.results['fail'] = {};
+const openPage = require('./open_page_ortmodel.js');
+const fs = require('fs');
+const args = require('yargs').argv;
+const path = require('path');
+
+async function runSingleBenchmark(benchmarkObject) {
+  const url = benchmarkObject['url'];
+  const repeat = benchmarkObject['repeat'];
+  const logFile = benchmarkObject['logFile'];
+  const model = benchmarkObject['model'];
+  let logDir = __dirname + '\\out\\'
+  //console.log(logFile)
+  for (let i = 0; i < repeat; i++) {
+    await openPage(url, logFile, model);
+  }
+}
+
+const w = '1111';
+
+async function runURL(modelName) {
+  const port1 = "8080";
+  const port2 = "8081"; // "should fail"
+  const url1 = `https://10.239.47.10:${port1}/ort-toolkit/?ortUrl=https://10.239.47.10:${port1}/onnxruntime&modelName=`;
+  const url1end = "&task=ortProfiling&ep=webgpu&modelUrl=wp-27&enableIoBinding=true&runTimes=1&warmupTimes=0&enableDebug=true&enableFreeDimensionOverrides=true&enableGraphCapture=true";
+
+
+  const url2 = `https://10.239.47.10:${port2}/ort-toolkit/?ortUrl=https://10.239.47.10:${port2}/onnxruntime&modelName=`;
+  const url2end = "&task=ortProfiling&ep=webgpu&modelUrl=wp-27&enableIoBinding=true&runTimes=1&warmupTimes=0&enableDebug=true&enableFreeDimensionOverrides=true&enableGraphCapture=true";
+
+    let finalUrl1 = url1 + modelName + url1end;
+    let finalUrl2 = url2 + modelName + url2end;
+    //console.log(finalUrl1);
+    //console.log(finalUrl2);
+    await runSingleBenchmark(
+       {'repeat': 1, 'url': finalUrl1, 'logFile': 'pass8080.txt', 'model': modelName});
+    await runSingleBenchmark(
+      {'repeat': 1, 'url': finalUrl2, 'logFile': 'fail8081.txt', 'model': modelName});
+    // Start waiting for download before clicking. Note no await.
+  return [0, 0];
+}
+
+function ensureDirectoryExistence(filePath) {
+  if (fs.existsSync(filePath)) {
+    return true;
+  }
+  fs.mkdirSync(filePath);
+}
+
+async function getURLFromCartesianProductJSON() {
+  for (let i = 91; i < 92; i++) {
+    global.results = {};
+    global.results['pass'] = {};
+    global.results['fail'] = {};
+    const modelName = 'whisper-tiny-decoder';
+    await runURL(modelName);
+    console.log(global.results['pass']);
+    console.log(global.results['fail']);
+    //ensureDirectoryExistence('./fusedata'+w);
+    //ensureDirectoryExistence('./fusedatano'+w);
+    //fs.writeFileSync('./fusedata'+w+'/data'+startValue+ '-'+ endValue+ '.json', JSON.stringify(global.results['fuse']));
+    //fs.writeFileSync('./fusedatano'+w+'/data'+startValue+ '-'+ endValue+ '.json', JSON.stringify(global.results['nofuse']));
+  }
+}
+
+(async function() {
+  await getURLFromCartesianProductJSON();
+  
+})();
---
+++ src/open_page_ortmodel.js
@@ -0,0 +1,117 @@
+'use strict';
+const {chromium} = require('playwright');
+// https://github.com/axinging/webtest/blob/support_tracing3/src/open_page.js
+let logStatus = {logEnd: false};
+
+async function waitForCondition(condition) {
+  return new Promise(resolve => {
+    var start_time = Date.now();
+    function checkCondition() {
+      if (condition.logEnd == true) {
+        condition.logEnd = false;
+        resolve();
+      } else if (Date.now() > start_time + 3600 * 1000) {
+        resolve();
+      } else {
+        setTimeout(checkCondition, 1000);
+      }
+    }
+    checkCondition();
+  });
+}
+
+// "C:\Users\abc\AppData\Local\Google\Chrome SxS\Application\chrome.exe" --enable-dawn-features=allow_unsafe_apis,use_dxc --enable-features=SharedArrayBuffer  --dump-shaders
+
+const browserPath =
+    `C:\\Users\\abc\\AppData\\Local\\Google\\Chrome SxS\\Application\\chrome.exe`;
+const userDataDir = `${process.env.LOCALAPPDATA}/Google/Chrome SxS/User Data`;
+
+function log(info, logFile) {
+  const fs = require('fs');
+  fs.appendFileSync(logFile, String(info) + '\n');
+}
+
+async function clickDownloadButton(){
+  const [newTab] = await Promise.all([
+      this.page.waitForEvent("popup"),
+      this.waitAndClick(this.elements.button_downloadButton)
+  ]);
+  return newTab;
+}
+
+async function startContext(exitCondition, logFile, model, url) {
+  let browserArgs =`--enable-dawn-features=allow_unsafe_apis,use_dxc --enable-features=SharedArrayBuffer`;
+  /*
+  --enable-dawn-features=record_detailed_timing_in_trace_events,disable_timestamp_query_conversion 
+  --enable-tracing=disabled-by-default-gpu.dawn  --trace-startup-file=${
+          tracingFile} --trace-startup-format=json`;
+  */
+  let context = await chromium.launchPersistentContext(userDataDir, {
+    headless: false,
+    executablePath: browserPath,
+    viewport: null,
+    ignoreHTTPSErrors: true,
+    args: browserArgs.split(' '),
+  });
+  let page = await context.newPage();
+  page.on('console', async msg => {
+    //for (let i = 0; i < msg.args().length; ++i) {
+    //  log(`[console] ${i}: ${await msg.args()[i].jsonValue()}`, logFile);
+    //}
+
+    // if (msg._event.text[0]== '{') {
+    //   if(logFile.startsWith('fuse')) {
+    //     const elem = logFile.split('.')[0].replace('fuse','');
+    //     global.results['fuse'][elem] = JSON.parse(msg._event.text);
+    //   } else {
+    //     const elem = logFile.split('.')[0].replace('nofuse','');
+    //     global.results['nofuse'][elem] = JSON.parse(msg._event.text);
+    //   }
+    // }
+    let msgStr = ('' + msg.args()[0]).replace('JSHandle@', '');
+    //console.log(msgStr);
+    if (msgStr.includes('ortend')) {
+        console.log(msgStr);
+        global.results['pass'][model] = 'pass';
+        exitCondition.logEnd = true;
+    }
+  });
+  page.on('pageerror', (err) => {
+    console.log("xxx" + err.message);
+    global.results['fail'][model] = err.message;
+    //console.log(msgStr);
+    exitCondition.logEnd = true;
+  });
+  return [context, page];
+}
+
+async function closeContext(context) {
+  await context.close();
+}
+
+async function openPage(url, logFile, model) {
+  if (url == '') {
+    throw 'URL is empty';
+  }
+  console.log(url);
+  const [context, page] = await startContext(logStatus, logFile, model, url);
+  await page.goto(url);
+  await waitForCondition(logStatus);
+
+  //const downloadPromise = page.waitForEvent('download');
+  // await page.getByText('Download file').click();
+  //const download = await downloadPromise;
+  // const downloadPromise = page.waitForEvent('download');
+  // console.log("xxx 1");
+  // // await page.getByText('Download file').click();
+  // console.log("xxx 2");
+  // const download = await downloadPromise;
+  // console.log("xxx 3 " + download.suggestedFilename());
+  // await download.saveAs('./' + download.suggestedFilename());
+
+  await closeContext(context);
+  return logFile;
+}
+
+
+module.exports = openPage;
